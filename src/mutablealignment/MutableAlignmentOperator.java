b8bb83f60bc343c161653b83fcc3098ce593c2ea
Yao Xiao <xbarry1818@gmail.com>
2025-03-20 14:34:55 +1300

    Create operator for MutableAlignment #1

---
+++ src/mutablealignment/MutableAlignmentOperator.java
@@ -0,0 +1,43 @@
+package mutablealignment;
+
+import beast.base.core.Description;
+import beast.base.core.Input;
+import beast.base.inference.Operator;
+import beast.base.util.Randomizer;
+
+@Description("This operator changes an alignment.")
+public class MutableAlignmentOperator extends Operator {
+    public Input<MutableAlignment> mutableAlignmentInput = new Input<>("mutableAlignment", "mutableAlignment");
+    private MutableAlignment mutableAlignment;
+    private Integer site;
+    private Integer taxa;
+    private Integer oldState;
+    private Integer newState;
+
+    public MutableAlignmentOperator() {}
+
+
+    @Override
+    public void initAndValidate() {
+        mutableAlignment = mutableAlignmentInput.get();
+    }
+
+
+    @Override
+    public double proposal() {
+        taxa = Randomizer.nextInt(mutableAlignment.getTaxonCount());
+        site = Randomizer.nextInt(mutableAlignment.getSiteCount());
+        oldState = mutableAlignment.getSiteValue(taxa, site);
+        do {
+            int numStates = mutableAlignment.getDataType().getStateCount();
+            newState = Randomizer.nextInt(numStates);
+        } while (newState.equals(oldState));
+        mutableAlignment.setSiteValue(taxa, site, newState);
+        return 0;
+    }
+
+
+
+
+
+}
